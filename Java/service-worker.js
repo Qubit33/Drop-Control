2bb7ac14aca661f4b10268eb49b9b0bed2ed684b
Qubit33 <patrickandradegt@gmail.com>
2025-06-24 11:34:46 -0300

    PWA Integration

---
+++ Java/service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', event => {
+  console.log('[SW] Instalado');
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './Source/assets/style.css',
+        './Java/Script.js',
+        './Audio/Lose-Control.mp3',
+        './Audio/Lose-Control.txt',
+        './Source/icon/icon-192.png',
+        './Source/icon/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
